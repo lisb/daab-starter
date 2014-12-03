cef5ce11a6b232488a0da9d09203ce6808d4808c
masataka.takeuchi <masataka.takeuchi@l-is-b.com>
2014-12-03 16:13:31 +0900

    adapter を direct に設定。

--- bin/hubot.cmd
+++ bin/hubot.cmd
@@ -1,4 +1,4 @@
 @echo off
 
-if not exist node_modules\hubot\README_direct.md npm install node_modules_pack\*
-npm install && node_modules\.bin\hubot.cmd %* 
+if not exist node_modules\hubot\README_direct.md npm install node_modules_pack\hubot-*
+npm install && node_modules\.bin\hubot.cmd -d -a direct %* 
