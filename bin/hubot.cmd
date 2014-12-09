8fee994a3b4bac972ad4480a9d8072d6d1fb9652
masataka.takeuchi <masataka.takeuchi@l-is-b.com>
2014-12-09 15:56:06 +0900

    プライベートモジュールは URL からダウンロードする。

--- bin/hubot.cmd
+++ bin/hubot.cmd
@@ -1,4 +1,4 @@
 @echo off
-
-if not exist node_modules\hubot\README_direct.md npm install node_modules_pack\hubot-*
+SET BASE=https://direct4b.com/ja/bot/dev/modules
+if not exist node_modules\hubot\README_direct.md npm install %BASE%/hubot.tgz
 npm install && node_modules\.bin\hubot.cmd -d -a direct %* 
