9a182eac0eb4eca7ade672771759d6c14c580fad
masataka.takeuchi <masataka.takeuchi@l-is-b.com>
2014-12-11 09:45:48 +0900

    windows で起動中に停止することがあったので修正。

--- bin/hubot.cmd
+++ bin/hubot.cmd
@@ -1,4 +1,12 @@
 @echo off
-SET BASE=https://direct4b.com/ja/bot/dev/modules
-if not exist node_modules\hubot\README_direct.md npm install %BASE%/hubot.tgz
-npm install && node_modules\.bin\hubot.cmd -d -a direct %* 
+setlocal
+
+rem installer bug. http://stackoverflow.com/a/25095327
+mkdir "%APPDATA%\npm" > NUL 2>&1
+
+set BASE=https://direct4b.com/ja/bot/dev/modules
+if not exist node_modules\hubot\README_direct.md call npm install %BASE%/hubot.tgz
+
+set PATH=node_modules\.bin;node_modules\hubot\node_modules\.bin;%PATH%
+call npm install
+call node_modules\.bin\hubot.cmd -d -a direct %*
