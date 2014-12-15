ec6f99a43d64767df81156790f2c6e0f8afac834
masataka.takeuchi <masataka.takeuchi@l-is-b.com>
2014-12-15 12:43:51 +0900

    依存関係をスクリプトではなく package.json に記述

--- bin/hubot.cmd
+++ bin/hubot.cmd
@@ -1,12 +1,7 @@
 @echo off
-setlocal
 
 rem installer bug. http://stackoverflow.com/a/25095327
 mkdir "%APPDATA%\npm" > NUL 2>&1
 
-set BASE=https://direct4b.com/ja/bot/dev/modules
-if not exist node_modules\hubot\README_direct.md call npm install %BASE%/hubot.tgz
-
 set PATH=node_modules\.bin;node_modules\hubot\node_modules\.bin;%PATH%
-call npm install
-call node_modules\.bin\hubot.cmd -d -a direct %*
+npm install && node_modules\.bin\hubot.cmd -d -a direct %*
