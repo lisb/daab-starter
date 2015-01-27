0b80b7727642162297d52e4d9116bd967782b161
masataka.takeuchi <masataka.takeuchi@l-is-b.com>
2015-01-28 02:05:52 +0900

    forever でのデーモン化。

--- bin/hubot.cmd
+++ bin/hubot.cmd
@@ -4,4 +4,8 @@ rem installer bug. http://stackoverflow.com/a/25095327
 mkdir "%APPDATA%\npm" > NUL 2>&1
 
 set PATH=node_modules\.bin;node_modules\hubot\node_modules\.bin;%PATH%
-npm install && node_modules\.bin\hubot.cmd -d -a direct %*
+if Defined HUBOT_DIRECT_TOKEN (
+  npm install && node_modules\.bin\hubot.cmd -a direct %*
+) else (
+  npm install && forever start --minUptime 1000 --spinSleepTime 1000 -o hubot.log -a -w -c coffee node_modules\.bin\hubot.cmd -a direct %*
+)
