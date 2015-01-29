22dd2a7544e1950b1769a4dfebae97995544a8b3
masataka.takeuchi <masataka.takeuchi@l-is-b.com>
2015-01-29 09:47:42 +0900

    forever 関連のコマンドが使えるように修正。

--- bin/hubot.cmd
+++ bin/hubot.cmd
@@ -4,8 +4,26 @@ rem installer bug. http://stackoverflow.com/a/25095327
 mkdir "%APPDATA%\npm" > NUL 2>&1
 
 set PATH=node_modules\.bin;node_modules\hubot\node_modules\.bin;%PATH%
-if Defined HUBOT_DIRECT_TOKEN (
-  npm install && node_modules\.bin\hubot.cmd -a direct %*
+call npm install
+
+for /f "delims=\" %%i in ("%CD%") do set UID=%%i
+
+if "%1" == "start" (
+  forever start ^
+    --uid "%UID%"
+    --minUptime 10000 --spinSleepTime 600000 ^
+    -o hubot.log -a ^
+    -w --watchDirectory scripts ^
+    -c coffee node_modules\.bin\hubot.cmd -a direct %*
+) else if "%1" == "stop" (
+  forever stop "%UID%"
+) else if "%1" == "restart" (
+  forever restart "%UID%"
+) else if "%1" == "status" (
+  forever list | find "] %UID% "
+) else if "%1" == "logs" (
+  cat hubot.log | more
 ) else (
-  npm install && forever start --minUptime 1000 --spinSleepTime 1000 -o hubot.log -a -w --watchDirectory scripts -c coffee node_modules\.bin\hubot.cmd -a direct %*
+  node_modules\.bin\hubot.cmd -a direct %*
 )
+
