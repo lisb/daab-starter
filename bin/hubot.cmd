1c533fbfa28c04010ce823563813fd0fcb058669
masataka.takeuchi <masataka.takeuchi@l-is-b.com>
2015-08-31 17:33:29 +0900

    Fix wrong PATH in bin/hubot.

--- bin/hubot.cmd
+++ bin/hubot.cmd
@@ -3,7 +3,7 @@
 rem installer bug. http://stackoverflow.com/a/25095327
 mkdir "%APPDATA%\npm" > NUL 2>&1
 
-set PATH=node_modules\.bin;node_modules\hubot\node_modules\.bin;%PATH%
+set PATH=node_modules\.bin;node_modules\lisb-hubot\node_modules\.bin;%PATH%
 call npm install
 
 for /f "delims=\" %%i in ("%CD%") do set UID=%%i
